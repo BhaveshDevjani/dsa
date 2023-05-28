a18173e40b5b5b05a3a672b4b262ea0c31aca857
Bhavesh Devjani <bhaveshdevjani14@gmail.com>
2023-05-28 19:04:22 +0530

    including Main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,6 @@
 package org.example;
 
-import org.example.binarytrees.BinaryTreeNode;
-import org.example.binarytrees.BinaryTreeUseHelper;
-
 public class Main {
     public static void main(String[] args) {
-//        1 2 3 4 5 6 7 -1 -1 -1 -1 -1 -1 -1 -1
-        BinaryTreeNode<Integer> root = BinaryTreeUseHelper.takeInputLevelWise();
-        System.out.println(BinaryTreeUseHelper.size(root));
     }
 }
